e05365da489d87693a6a4ca0319d488176799749
jyotiska <jyotiska123@gmail.com>
2014-05-11 04:41:54 +0530

    modified voterwise data scraping

--- Hebbal_Data/indi2data.py
+++ Hebbal_Data/indi2data.py
@@ -32,3 +32,65 @@ for item in range(1580001, 1580213):
 outfile = open("fulldata.json", "w")
 outfile.write(json.dumps(finaldata, indent=4))
 outfile.close()
+
+final_voter_data = []
+
+for j in range(1580001, 1580212):
+    pdf2txt.main(["", "-M 100", "-ob.txt", "pdf/" + str(j) + ".pdf"])
+    infile = open("b.txt").readlines()
+
+    globallist = []
+    locallist = []
+    record = 0
+
+    for line in infile:
+        if line.startswith("SOH") and len(locallist) != 0:
+            globallist.append(locallist)
+            locallist = []
+            locallist.append(line)
+            record = 1
+        elif line.startswith("SOH") and len(globallist) == 0:
+            locallist.append(line)
+            record = 1
+        elif not line.startswith("SOH") and record == 1:
+            locallist.append(line)
+
+    locallist = []
+    for line in infile:
+        if line.startswith("BCW") and len(locallist) != 0:
+            globallist.append(locallist)
+            locallist = []
+            locallist.append(line)
+            record = 1
+        elif line.startswith("BCW") and len(globallist) == 0:
+            locallist.append(line)
+            record = 1
+        elif not line.startswith("BCW") and record == 1:
+            locallist.append(line)
+
+    for item in globallist:
+        try:
+            new_list = []
+            t = collections.OrderedDict()
+            for i in xrange(len(item)):
+                if item[i].strip():
+                    new_list.append(item[i].strip())
+            t["id"] = new_list[0]
+            t["name"] = new_list[1]
+            t["f/m/h name"] = new_list[2]
+            for el in new_list:
+                if el.startswith("House"):
+                    t["house no"] = el.split(":")[1]
+                elif el.startswith("Sex"):
+                    t["sex"] = el.split("Age")[0].split(":")[1].strip()
+                    t["age"] = el.split("Age")[1].split(":")[1].strip()
+            final_voter_data.append(t)
+        except:
+            print "error"
+
+    print str(j), "complete"
+
+print len(final_voter_data)
+outfile = open("fulldata_voterwise.json", "w")
+outfile.write(json.dumps(final_voter_data, indent=4))
+outfile.close()
